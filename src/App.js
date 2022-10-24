3857bdcff577e045028ab792de4d8a9f71656719
Muhammed Safeer <mhdsafeert123@gmail.com>
2022-10-24 21:58:42 +0530

    added urls & modified post's

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from './Components/NavBar/NavBar';
 import './App.css'
-import {original,action} from './urls' 
+import {original,action,topRated, upcoming, comedy, horror, romance, documentaries} from './urls' 
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
 
@@ -11,7 +11,14 @@ function App() {
       <NavBar/>
       <Banner/>
       <RowPost url={original} title='Netflix Originals' />
+      <RowPost url={upcoming} title='UpComming' isSmall />
+      <RowPost url={topRated} title='Top Rated' isSmall />
       <RowPost url={action} title='Action' isSmall />
+      <RowPost url={comedy} title='Comedy' isSmall />
+      <RowPost url={horror} title='Horror' isSmall />
+      <RowPost url={romance} title='Romance' isSmall />
+      <RowPost url={documentaries} title='Documentaries' isSmall />
+
     </div>
   );
 }
